7e94ab451e82212cc03c64f9725346bc681dfa5f
windet01 <theo.windebank@bbc.co.uk>
2018-07-17 15:21:38 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 if __name__ == "__main__":
     setup(
         name="stackdriver_logging",
-        version="0.1.1",
+        version="0.1.2",
         author="Datalab",
         author_email="datalab@bbc.co.uk",
         description="Adds distributed tracing information to logger output and sends traces to the Stackdriver "

e8ed946577a270865795a08f38f676f0608c12ae
windet01 <theo.windebank@bbc.co.uk>
2018-07-31 18:40:20 +0100

    fix tests

--- tests/test_helpers_flask_callbacks.py
+++ tests/test_helpers_flask_callbacks.py
@@ -4,6 +4,7 @@
 # set up with drone
 from unittest.mock import patch, MagicMock
 
+import pytest
 from pytest import mark
 
 from logtracer.helpers.flask.callbacks import start_span_and_log_request_before, log_response_after, \
@@ -13,9 +14,7 @@ from logtracer.helpers.flask.callbacks import start_span_and_log_request_before,
 @patch('logtracer.helpers.flask.callbacks.start_traced_span')
 @patch('logtracer.helpers.flask.callbacks.get_logger')
 @patch('logtracer.helpers.flask.callbacks.request')
-@patch('logtracer.helpers.flask.callbacks._is_path_excluded')
-def test_start_span_and_log_request_before(m_path_exclude, m_request, m_get_logger, m_start_traced_span):
-    m_path_exclude.return_value = False
+def test_start_span_and_log_request_before(m_request, m_get_logger, m_start_traced_span):
     m_request.headers = 'test_headers'
     m_request.method = 'test_method'
     m_request.path = 'test_path'
@@ -23,34 +22,17 @@ def test_start_span_and_log_request_before(m_path_exclude, m_request, m_get_logg
     m_logger = MagicMock()
     m_get_logger.return_value = m_logger
 
-    execute_before_request = start_span_and_log_request_before(['excluded_routes'], ['excluded_partial_routes'])
+    execute_before_request = start_span_and_log_request_before()
     execute_before_request()
 
-    m_path_exclude.assert_called_with(['excluded_routes'], ['excluded_partial_routes'])
     m_start_traced_span.assert_called_with('test_headers', 'test_path')
     m_logger.info.assert_called_with('test_method - test_url')
 
 
-@patch('logtracer.helpers.flask.callbacks.start_traced_span')
-@patch('logtracer.helpers.flask.callbacks.get_logger')
-@patch('logtracer.helpers.flask.callbacks._is_path_excluded')
-def test_start_span_and_log_request_before_excluded_route(m_path_exclude, m_get_logger, m_start_traced_span):
-    m_path_exclude.return_value = True
-
-    execute_before_request = start_span_and_log_request_before(['excluded_routes'], ['excluded_partial_routes'])
-    execute_before_request()
-
-    m_path_exclude.assert_called_with(['excluded_routes'], ['excluded_partial_routes'])
-    assert not m_start_traced_span.called
-    assert not m_get_logger.called
-
-
-@patch('logtracer.helpers.flask.callbacks._is_path_excluded')
 @patch('logtracer.helpers.flask.callbacks.get_logger')
 @patch('logtracer.helpers.flask.callbacks.request')
 @mark.parametrize('success_status_code', [200, 201, 300, 301])
-def test_log_response_after_success(m_request, m_get_logger, m_path_exclude, success_status_code):
-    m_path_exclude.return_value = False
+def test_log_response_after_success(m_request, m_get_logger, success_status_code):
     m_logger = MagicMock()
     m_get_logger.return_value = m_logger
     m_request.url = 'test_url'
@@ -58,20 +40,17 @@ def test_log_response_after_success(m_request, m_get_logger, m_path_exclude, suc
     m_response.status_code = success_status_code
     m_response.status = 'test_status'
 
-    execute_after_request = log_response_after(['excluded_routes'], ['excluded_partial_routes'])
+    execute_after_request = log_response_after()
     execute_after_request(m_response)
 
-    m_path_exclude.assert_called_with(['excluded_routes'], ['excluded_partial_routes'])
     m_logger.info.assert_called_with('test_status - test_url')
     assert not m_logger.error.called
 
 
-@patch('logtracer.helpers.flask.callbacks._is_path_excluded')
 @patch('logtracer.helpers.flask.callbacks.get_logger')
 @patch('logtracer.helpers.flask.callbacks.request')
 @mark.parametrize('error_status_code', [400, 401, 500, 501])
-def test_log_response_after_error(m_request, m_get_logger, m_path_exclude, error_status_code):
-    m_path_exclude.return_value = False
+def test_log_response_after_error(m_request, m_get_logger, error_status_code):
     m_logger = MagicMock()
     m_get_logger.return_value = m_logger
     m_request.url = 'test_url'
@@ -79,25 +58,11 @@ def test_log_response_after_error(m_request, m_get_logger, m_path_exclude, error
     m_response.status_code = error_status_code
     m_response.status = 'test_status'
 
-    execute_after_request = log_response_after(['excluded_routes'], ['excluded_partial_routes'])
+    execute_after_request = log_response_after()
     execute_after_request(m_response)
 
-    m_path_exclude.assert_called_with(['excluded_routes'], ['excluded_partial_routes'])
     m_logger.error.assert_called_with('test_status - test_url')
     assert not m_logger.info.called
-
-
-@patch('logtracer.helpers.flask.callbacks._is_path_excluded')
-@patch('logtracer.helpers.flask.callbacks.get_logger')
-@mark.parametrize('error_status_code', [400, 401, 500, 501])
-def test_log_response_after_excluded_route(m_get_logger, m_path_exclude, error_status_code):
-    m_path_exclude.return_value = True
-
-    execute_after_request = log_response_after(['excluded_routes'], ['excluded_partial_routes'])
-    execute_after_request(MagicMock())
-
-    m_path_exclude.assert_called_with(['excluded_routes'], ['excluded_partial_routes'])
-    assert not m_get_logger.info.called
 
 
 @patch('logtracer.helpers.flask.callbacks._is_path_excluded')
@@ -109,19 +74,19 @@ def test_close_span_on_teardown(m_end_traced_span, m_path_exclude):
     execute_on_teardown(MagicMock())
 
     m_path_exclude.assert_called_with(['excluded_routes'], ['excluded_partial_routes'])
-    assert m_end_traced_span.called
+    m_end_traced_span.assert_called_with(False)
 
 
 @patch('logtracer.helpers.flask.callbacks._is_path_excluded')
 @patch('logtracer.helpers.flask.callbacks.end_traced_span')
-def test_close_span_on_teardown_excluded_route(m_end_traced_span, m_path_exclude):
-    m_path_exclude.return_value = True
+def test_close_span_on_teardown(m_end_traced_span, m_path_exclude):
+    m_path_exclude.return_value = False
 
     execute_on_teardown = close_span_and_post_on_teardown(['excluded_routes'], ['excluded_partial_routes'])
     execute_on_teardown(MagicMock())
 
     m_path_exclude.assert_called_with(['excluded_routes'], ['excluded_partial_routes'])
-    assert not m_end_traced_span.called
+    m_end_traced_span.assert_called_with(True)
 
 
 @patch('logtracer.helpers.flask.callbacks.request')
@@ -152,3 +117,14 @@ def test_is_path_excluded_excluded(m_request):
 
     m_request.path = '/excluded'
     assert _is_path_excluded(['/excluded'], ['/also_excluded'])
+
+
+def test_is_path_excluded_error():
+    with pytest.raises(ValueError):
+        assert _is_path_excluded('test_string', None)
+
+    with pytest.raises(ValueError):
+        assert _is_path_excluded(None, 'test_string')
+
+    with pytest.raises(ValueError):
+        assert _is_path_excluded('test_string', 'test_string')
--- tests/test_helpers_flask_decorators.py
+++ tests/test_helpers_flask_decorators.py
@@ -1,50 +1,6 @@
-from unittest.mock import MagicMock, patch, call
+from unittest.mock import MagicMock, patch
 
-from logtracer.helpers.flask.decorators import trace_and_log_route, log_exception_and_close_span, log_exception
-
-
-@patch('logtracer.helpers.flask.decorators.start_traced_span')
-@patch('logtracer.helpers.flask.decorators.end_traced_span')
-@patch('logtracer.helpers.flask.decorators.get_logger')
-@patch('logtracer.helpers.flask.decorators.request')
-def test_trace_and_log_route(m_request, m_get_logger, m_end_traced_span, m_start_traced_span):
-    m_route, m_response, m_logger = MagicMock(), MagicMock(), MagicMock()
-    m_get_logger.return_value = m_logger
-    m_response.status = 'test_response_status'
-    m_route.return_value = m_response, 'test_status_number'
-    m_request.headers = 'test_headers'
-    m_request.path = 'test_path'
-    m_request.method = 'test_method'
-    m_request.url = 'test_url'
-
-    wrapper = trace_and_log_route(m_route)
-    wrapper('arg', kwarg='kwarg')
-
-    m_start_traced_span.assert_called_with('test_headers', 'test_path')
-    expected_logs = [call('test_method - test_url'), call('test_response_status - test_url')]
-    assert m_logger.info.call_args_list == expected_logs
-    assert m_route.called_with('arg', kwarg='kwarg')
-    assert m_end_traced_span.called
-
-
-@patch('logtracer.helpers.flask.decorators.end_traced_span')
-@patch('logtracer.helpers.flask.decorators.get_logger')
-@patch('logtracer.helpers.flask.decorators.request')
-def test_trace_and_log_exception(m_request, m_get_logger, m_end_traced_span):
-    m_exception_handler, m_exception, m_response, m_logger = MagicMock(), MagicMock(), MagicMock(), MagicMock()
-    m_get_logger.return_value = m_logger
-    m_response.status = 'test_response_status'
-    m_exception_handler.return_value = m_response
-    m_request.url = 'test_url'
-
-    wrapper = log_exception_and_close_span(m_exception_handler)
-    response = wrapper(m_exception)
-
-    m_exception_handler.assert_called_with(m_exception)
-    m_logger.exception.assert_called_with(m_exception)
-    m_logger.error.assert_called_with('test_response_status - test_url')
-    assert m_end_traced_span.called
-    assert response == m_response
+from logtracer.helpers.flask.decorators import log_exception
 
 
 @patch('logtracer.helpers.flask.decorators.get_logger')

34ffca46b007f9f79dc4144c23c0b27a57339489
windet01 <theo.windebank@bbc.co.uk>
2018-08-08 09:41:20 +0100

    update outdated docstring

--- logtracer/_global_vars.py
+++ logtracer/_global_vars.py
@@ -1,4 +1,7 @@
-"""Variables to be used across tracing and logging. These are set by jsonlog.configurelogging.py."""
+"""Variables to be used across tracing and logging."""
+# set by jsonlog.configure_json_logging
 gcp_project_name = ''
 service_name = ''
+
+# set by tracing.configure_tracing
 post_spans_to_stackdriver_api = False

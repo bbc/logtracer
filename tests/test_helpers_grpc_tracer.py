5106cf7d6ec9144ae6324d18ef2bcbf485b0c5f2
windet01 <theo.windebank@bbc.co.uk>
2018-08-20 12:00:53 +0100

    more interceptor unit tests

--- tests/test_helpers_grpc_tracer.py
+++ tests/test_helpers_grpc_tracer.py
@@ -1,9 +1,10 @@
-from logging import exception
+import json
 from unittest.mock import MagicMock, patch, call
 
 import pytest
+from grpc._cython.cygrpc import _Metadatum
 
-from logtracer.helpers.grpc.tracer import GRPCTracer, _IncomingInterceptor, _OutgoingInterceptor
+from logtracer.helpers.grpc.tracer import GRPCTracer, _IncomingInterceptor, _OutgoingInterceptor, B3_VALUES_KEY
 
 
 def test_GRPCTracer_init():
@@ -15,10 +16,15 @@ def test_GRPCTracer_init():
     assert grpc_tracer.redacted_fields == 'test_redacted_fields'
 
 
+def test_IncomingInterceptor_init():
+    m_tracer = MagicMock()
+    assert _IncomingInterceptor(m_tracer)._tracer == m_tracer
+
+
 @patch('logtracer.helpers.grpc.tracer._wrap_rpc_behavior', MagicMock(side_effect=lambda handler, fn: fn))
 @patch('logtracer.helpers.grpc.tracer.redact_request', return_value='test_redacted_request')
 @patch('logtracer.helpers.grpc.tracer._grpc_status_from_context', return_value='.test_grpc_status')
-def test_IncomingInterceptor(m_grpc_status, m_redact):
+def test_IncomingInterceptor_intercept_service(m_grpc_status, m_redact):
     m_tracer = MagicMock()
     interceptor = _IncomingInterceptor(m_tracer)
     interceptor._retrieve_span_values_from_incoming_call = MagicMock(return_value='test_b3_values')
@@ -49,7 +55,7 @@ def test_IncomingInterceptor(m_grpc_status, m_redact):
 @patch('logtracer.helpers.grpc.tracer._wrap_rpc_behavior', MagicMock(side_effect=lambda handler, fn: fn))
 @patch('logtracer.helpers.grpc.tracer.redact_request', return_value='test_redacted_request')
 @patch('logtracer.helpers.grpc.tracer._grpc_status_from_context', return_value='.test_grpc_status')
-def test_IncomingInterceptor_exception(m_grpc_status, m_redact):
+def test_IncomingInterceptor_intercept_service_exception(m_grpc_status, m_redact):
     class TestException(Exception):
         pass
 
@@ -82,3 +88,58 @@ def test_IncomingInterceptor_exception(m_grpc_status, m_redact):
     )
     interceptor._tracer.logger.exception.assert_called_with(m_exception)
     interceptor._tracer.end_traced_span.assert_called_with(exclude_from_posting=False)
+
+
+def test_IncomingInterceptor_retrieve_span_values_from_incoming_call():
+    m_handler_call_details = MagicMock()
+    m_handler_call_details.invocation_metadata = [
+        _Metadatum(key=B3_VALUES_KEY, value=json.dumps({'test_b3_values': 'values'})),
+        _Metadatum(key='other_key', value='test_other_value')
+    ]
+
+    values = _IncomingInterceptor._retrieve_span_values_from_incoming_call(m_handler_call_details)
+
+    assert values == {'test_b3_values': 'values'}
+
+
+def test_IncomingInterceptor_retrieve_span_values_from_incoming_call_no_values():
+    m_handler_call_details = MagicMock()
+    m_handler_call_details.invocation_metadata = [
+        _Metadatum(key='other_key', value='test_other_value')
+    ]
+
+    values = _IncomingInterceptor._retrieve_span_values_from_incoming_call(m_handler_call_details)
+
+    assert values == {}
+
+
+def test_OutgoingInterceptor_init():
+    m_tracer = MagicMock()
+    assert _OutgoingInterceptor(m_tracer)._tracer == m_tracer
+
+
+def test_OutgoingInterceptor_intercept_unary_unary():
+    m_tracer = MagicMock()
+    interceptor = _OutgoingInterceptor(m_tracer)
+    interceptor._generate_metadata_with_b3_values = MagicMock(return_value='test_metadata_with_b3_values')
+    m_continuation, m_client_call_details, m_request = MagicMock(), MagicMock(), MagicMock()
+    m_response_future = MagicMock()
+    m_continuation.return_value = m_response_future
+    m_client_call_details.method = 'test_method'
+    m_client_call_details.timeout = 'test_timeout'
+    m_client_call_details.credentials = 'test_credentials'
+
+    response_future = interceptor.intercept_unary_unary(m_continuation, m_client_call_details, m_request)
+
+    expected_logs = [
+        call('test_method - outbound gRPC call'),
+        call('Response received from test_method')
+    ]
+    assert interceptor._tracer.logger.info.call_args_list == expected_logs
+    modified_client_call_details = m_continuation.call_args[0][0]
+    assert modified_client_call_details.method == 'test_method'
+    assert modified_client_call_details.timeout == 'test_timeout'
+    assert modified_client_call_details.credentials == 'test_credentials'
+    assert modified_client_call_details.metadata == 'test_metadata_with_b3_values'
+    assert m_response_future.result.called
+    assert response_future == m_response_future

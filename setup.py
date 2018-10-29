6e5c6bdca5def19a75a3bff8b1b1bf85cd74c537
windet01 <theo.windebank@bbc.co.uk>
2018-10-29 16:20:21 +0000

    add protobuf to all reqs

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
             'flake8==3.5.0',
             'pycodestyle<2.4.0',
             'pytest-cov==2.5.1',
-
+            'protobuf>=3.6.0'
         ],
         tests_require=[
             'pytest==3.6.2',
@@ -43,6 +43,8 @@ if __name__ == "__main__":
             'flake8==3.5.0',
             'pycodestyle<2.4.0',
             'pytest-cov==2.5.1',
-            'flask==1.0.2'
+            'flask==1.0.2',
+            'protobuf>=3.6.0'
+
         ],
     )

79430be77830f41d561aaaccaefcf70b34cf22ae
windet01 <theo.windebank@bbc.co.uk>
2018-10-29 16:25:11 +0000

    try installing google

--- setup.py
+++ setup.py
@@ -30,12 +30,6 @@ if __name__ == "__main__":
         test_suite="tests",
         setup_requires=[
             'wheel',
-            'pytest==3.6.2',
-            'pytest-runner==4.2',
-            'flake8==3.5.0',
-            'pycodestyle<2.4.0',
-            'pytest-cov==2.5.1',
-            'protobuf>=3.6.0'
         ],
         tests_require=[
             'pytest==3.6.2',
@@ -44,6 +38,5 @@ if __name__ == "__main__":
             'pycodestyle<2.4.0',
             'pytest-cov==2.5.1',
             'flask==1.0.2',
-            'protobuf>=3.6.0'
         ],
     )
